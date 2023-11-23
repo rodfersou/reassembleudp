7aaf206872157d00c137e5188faeae4a78d533ee
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2023-11-23 15:01:51 -0300

    Add nix

---
+++ shell.nix
@@ -0,0 +1,27 @@
+with import <nixpkgs> {};
+pkgs.mkShell {
+    name = "go-shell";
+    packages = [
+        (python311.withPackages(ps: with ps; [
+            supervisor
+        ]))
+        go_1_20
+        nodejs-19_x
+        mongodb-6_0
+        lazygit
+        ripgrep
+        tree
+        pre-commit
+        delve
+        entr
+    ];
+
+    shellHook = ''
+        echo "Starting Go development environment"
+
+        export GOPATH=$ENV_DIR/.direnv
+        export PATH="$GOPATH/bin:$ENV_DIR/node_modules/.bin:$PATH"
+
+        go mod download
+    '';
+}
