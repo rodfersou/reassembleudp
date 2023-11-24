d6682ceb09f600d4d3b83607d86b0792111e077a
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2023-11-24 10:37:28 -0300

    Review and simplify

--- reassembleudp.go
+++ reassembleudp.go
@@ -17,12 +17,12 @@ import (
 )
 
 type Payload struct {
-	eof            int
-	flags          int
-	data_size      int
-	offset         int
-	transaction_id int
-	data           []int
+	TransactionId int   `bson:"transaction_id"`
+	Offset        int   `bson:"offset"`
+	DataSize      int   `bson:"data_size"`
+	Eof           int   `bson:"eof"`
+	Flags         int   `bson:"flags"`
+	Data          []int `bson:"data"`
 }
 
 func main() {
@@ -95,53 +95,13 @@ func worker(id int, conn net.PacketConn, coll *mongo.Collection, ctx context.Con
 	}
 }
 
-func createPayload(buf []byte) *Payload {
-	payload := Payload{
-		flags:          int(big.NewInt(0).SetBytes([]byte{buf[0] & 127, buf[1]}).Uint64()),
-		data_size:      int(big.NewInt(0).SetBytes(buf[2:4]).Uint64()),
-		offset:         int(big.NewInt(0).SetBytes(buf[4:8]).Uint64()),
-		transaction_id: int(big.NewInt(0).SetBytes(buf[8:12]).Uint64()),
-	}
-	if buf[0]&128 == 128 {
-		payload.eof = 1
-	} else {
-		payload.eof = 0
-	}
-	payload.data = []int{}
-	if len(buf) >= 12+payload.data_size {
-		payload.data = make([]int, payload.data_size)
-		for i, n := range buf[12 : 12+payload.data_size] {
-			payload.data[i] = int(n)
-		}
-	}
-	return &payload
-}
-
 func db_inserter(id int, coll *mongo.Collection, ctx context.Context, payloads <-chan *Payload) {
 	models := make([]mongo.WriteModel, 1024)
 	i := 0
 	tid := 1
-	defer func() {
-		_, err := coll.BulkWrite(ctx, models, options.BulkWrite().SetOrdered(false))
-		if err != nil {
-			panic(err)
-		}
-		// fmt.Println(
-		//  id,
-		//  " Inserting! ",
-		//  len(models),
-		// )
-		fmt.Println("Last transaction ", tid)
-	}()
 	for payload := range payloads {
 		models[i] = mongo.NewInsertOneModel().SetDocument(
-			bson.M{
-				"transaction_id": payload.transaction_id,
-				"offset":         payload.offset,
-				"data_size":      payload.data_size,
-				"eof":            payload.eof,
-				"data":           payload.data,
-			},
+			payload,
 		)
 
 		i++
@@ -150,9 +110,9 @@ func db_inserter(id int, coll *mongo.Collection, ctx context.Context, payloads <
 			if err != nil {
 				panic(err)
 			}
-			if payload.transaction_id != tid {
-				fmt.Println("Finished transactions from ", tid, "to ", payload.transaction_id-1)
-				tid = payload.transaction_id
+			if payload.TransactionId != tid {
+				fmt.Println("Finished transactions from ", tid, "to ", payload.TransactionId-1)
+				tid = payload.TransactionId
 			}
 			// fmt.Println(
 			//  id,
@@ -163,4 +123,38 @@ func db_inserter(id int, coll *mongo.Collection, ctx context.Context, payloads <
 			models = make([]mongo.WriteModel, 1024)
 		}
 	}
+	if i > 0 {
+		_, err := coll.BulkWrite(ctx, models, options.BulkWrite().SetOrdered(false))
+		if err != nil {
+			panic(err)
+		}
+		// fmt.Println(
+		//  id,
+		//  " Inserting! ",
+		//  len(models),
+		// )
+		fmt.Println("Last transaction ", tid)
+	}
+}
+
+func createPayload(buf []byte) *Payload {
+	payload := Payload{
+		Flags:         int(big.NewInt(0).SetBytes([]byte{buf[0] & 127, buf[1]}).Uint64()),
+		DataSize:      int(big.NewInt(0).SetBytes(buf[2:4]).Uint64()),
+		Offset:        int(big.NewInt(0).SetBytes(buf[4:8]).Uint64()),
+		TransactionId: int(big.NewInt(0).SetBytes(buf[8:12]).Uint64()),
+	}
+	if buf[0]&128 == 128 {
+		payload.Eof = 1
+	} else {
+		payload.Eof = 0
+	}
+	payload.Data = []int{}
+	if len(buf) >= 12+payload.DataSize {
+		payload.Data = make([]int, payload.DataSize)
+		for i, n := range buf[12 : 12+payload.DataSize] {
+			payload.Data[i] = int(n)
+		}
+	}
+	return &payload
 }
--- reassembleudp_test.go
+++ reassembleudp_test.go
@@ -8,115 +8,115 @@ import (
 func TestCreatePayloadEOF(t *testing.T) {
 	buf := make([]byte, 12)
 	payload := createPayload(buf)
-	assert.Equal(t, payload.eof, 0)
+	assert.Equal(t, payload.Eof, 0)
 
 	buf[0] = 255
 	payload = createPayload(buf)
-	assert.Equal(t, payload.eof, 1)
+	assert.Equal(t, payload.Eof, 1)
 
 	buf[0] = 128
 	payload = createPayload(buf)
-	assert.Equal(t, payload.eof, 1)
+	assert.Equal(t, payload.Eof, 1)
 
 	buf[0] = 127
 	payload = createPayload(buf)
-	assert.Equal(t, payload.eof, 0)
+	assert.Equal(t, payload.Eof, 0)
 }
 
 func TestCreatePayloadFlags(t *testing.T) {
 	buf := make([]byte, 12)
 	payload := createPayload(buf)
-	assert.Equal(t, payload.flags, 0)
+	assert.Equal(t, payload.Flags, 0)
 
 	buf[0] = 128
 	payload = createPayload(buf)
-	assert.Equal(t, payload.flags, 0)
+	assert.Equal(t, payload.Flags, 0)
 
 	buf[0] = 1
 	payload = createPayload(buf)
-	assert.Equal(t, payload.flags, 256)
+	assert.Equal(t, payload.Flags, 256)
 
 	buf[0] = 0
 	buf[1] = 1
 	payload = createPayload(buf)
-	assert.Equal(t, payload.flags, 1)
+	assert.Equal(t, payload.Flags, 1)
 }
 
 func TestCreatePayloadDataSize(t *testing.T) {
 	buf := make([]byte, 12)
 	payload := createPayload(buf)
-	assert.Equal(t, payload.data_size, 0)
+	assert.Equal(t, payload.DataSize, 0)
 
 	buf = make([]byte, 12+1)
 	buf[3] = 1
 	payload = createPayload(buf)
-	assert.Equal(t, payload.data_size, 1)
+	assert.Equal(t, payload.DataSize, 1)
 
 	buf = make([]byte, 12+256)
 	buf[2] = 1
 	payload = createPayload(buf)
-	assert.Equal(t, payload.data_size, 256)
+	assert.Equal(t, payload.DataSize, 256)
 }
 
 func TestCreatePayloadOffset(t *testing.T) {
 	buf := make([]byte, 12)
 	payload := createPayload(buf)
-	assert.Equal(t, payload.offset, 0)
+	assert.Equal(t, payload.Offset, 0)
 
 	buf[7] = 1
 	payload = createPayload(buf)
-	assert.Equal(t, payload.offset, 1)
+	assert.Equal(t, payload.Offset, 1)
 
 	buf[6] = 1
 	buf[7] = 0
 	payload = createPayload(buf)
-	assert.Equal(t, payload.offset, 256)
+	assert.Equal(t, payload.Offset, 256)
 
 	buf[5] = 1
 	buf[6] = 0
 	payload = createPayload(buf)
-	assert.Equal(t, payload.offset, 65536)
+	assert.Equal(t, payload.Offset, 65536)
 
 	buf[4] = 1
 	buf[5] = 0
 	payload = createPayload(buf)
-	assert.Equal(t, payload.offset, 16777216)
+	assert.Equal(t, payload.Offset, 16777216)
 }
 
 func TestCreatePayloadTransactionId(t *testing.T) {
 	buf := make([]byte, 12)
 	payload := createPayload(buf)
-	assert.Equal(t, payload.transaction_id, 0)
+	assert.Equal(t, payload.TransactionId, 0)
 
 	buf[11] = 1
 	payload = createPayload(buf)
-	assert.Equal(t, payload.transaction_id, 1)
+	assert.Equal(t, payload.TransactionId, 1)
 
 	buf[10] = 1
 	buf[11] = 0
 	payload = createPayload(buf)
-	assert.Equal(t, payload.transaction_id, 256)
+	assert.Equal(t, payload.TransactionId, 256)
 
 	buf[9] = 1
 	buf[10] = 0
 	payload = createPayload(buf)
-	assert.Equal(t, payload.transaction_id, 65536)
+	assert.Equal(t, payload.TransactionId, 65536)
 
 	buf[8] = 1
 	buf[9] = 0
 	payload = createPayload(buf)
-	assert.Equal(t, payload.transaction_id, 16777216)
+	assert.Equal(t, payload.TransactionId, 16777216)
 }
 
 func TestCreatePayloadData(t *testing.T) {
 	buf := make([]byte, 12)
 	payload := createPayload(buf)
-	assert.Equal(t, payload.data, []int{})
+	assert.Equal(t, payload.Data, []int{})
 
 	// If buffer size is smaller than data_size, data is empty
 	buf[3] = 3
 	payload = createPayload(buf)
-	assert.Equal(t, payload.data, []int{})
+	assert.Equal(t, payload.Data, []int{})
 
 	buf = make([]byte, 12+3)
 	buf[3] = 3
@@ -124,5 +124,5 @@ func TestCreatePayloadData(t *testing.T) {
 	buf[13] = 2
 	buf[14] = 3
 	payload = createPayload(buf)
-	assert.Equal(t, payload.data, []int{1, 2, 3})
+	assert.Equal(t, payload.Data, []int{1, 2, 3})
 }
